3c025ce646d086898ddc7d249e9356b3a6ae59ec
jimbou <el17072@mail.ntua.gr>
2022-11-29 14:59:42 +0200

    Added unchanged exe rapl measuring

--- find_trace.sh
+++ find_trace.sh
@@ -9,9 +9,16 @@ touch rapl_original.txt
 rm rapl_last.txt 2> /dev/null
 touch rapl_last.txt
 
+rm first_last_rapl.txt 2> /dev/null
+touch first_last_rapl.txt
+
+./general_rapl >> first_last_rapl.txt #store starting rapl value  of original exe without rapl reads
+./$1_unchanged  
+./general_rapl >> first_last_rapl.txt #store finishing rapl value  of original exe without rapl reads
+
 clang -o general_rapl general_rapl.c
-./general_rapl >> rapl_original.txt #store starting rapl value
+./general_rapl >> rapl_original.txt #store starting rapl value of the pass exe
 perf record -e intel_pt//u ./$1
-./general_rapl >> rapl_last.txt #store finishing rapl value
+./general_rapl >> rapl_last.txt #store finishing rapl value of the pass exe
 perf script --insn-trace --xed > trace.txt
 cp trace.txt $1
--- run_pass.sh
+++ run_pass.sh
@@ -6,10 +6,13 @@
 clang -S -emit-llvm $1.c
 llvm-as $1.ll
 
+llc -filetype=obj $1.bc
+clang  -o $1_unchanged $1.o  -O0 -static 
+
 opt -load build/skeleton/libSkeletonPass.* -skeleton < $1.bc > $1_inst.bc  -enable-new-pm=0  2> code_ll_$1.txt # to enable new pm mono locally oxi ston server
 llc -filetype=obj  $1_inst.bc 
 llvm-dis -o $1-debug.ll $1_inst.bc 
-clang  -o $1 $1_inst.o  -O0  
+clang  -o $1 $1_inst.o  -O0 -static 
 chmod +x $1
  echo "Creation of modified  executable successfull"
 
